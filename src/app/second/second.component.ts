59de4c95a60a036de3be95b11d04bb1d885feb42
brancaccioeros <brancaccio.eros@iisgalvanimi.edu.it>
2026-02-12 08:47:46 +0100

    Create second.component.ts file

---
+++ src/app/second/second.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-second',
+  templateUrl: './second.component.html',
+  styleUrls: ['./second.component.css']
+})
+export class SecondComponent {
+  brancaccio = {
+    app: 'app',
+    secondo: 'secondo',
+    terzo: 'terzo',
+    quarto: 'quarto'
+  };
+}
