54e51abbc134f9328019586474ceccc39727d66f
brancaccioeros <brancaccio.eros@iisgalvanimi.edu.it>
2026-02-12 08:48:39 +0100

    Add FourthComponent file

---
+++ src/app/fourth/fourth.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+@Component({
+  selector: 'app-fourth',
+  templateUrl: './fourth.component.html',
+  styleUrls: ['./fourth.component.css']
+})
+export class FourthComponent {
+  brancaccio = {
+    app: 'app',
+    secondo: 'secondo',
+    terzo: 'terzo',
+    quarto: 'quarto'
+  };
+}
