07c5593700c465cb6db47bd347c239651fc99b50
brancaccioeros <brancaccio.eros@iisgalvanimi.edu.it>
2026-02-12 08:47:10 +0100

    Create app.component.ts file

---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  brancaccio = {
+    app: 'app',
+    secondo: 'secondo',
+    terzo: 'terzo',
+    quarto: 'quarto'
+  };
+}
