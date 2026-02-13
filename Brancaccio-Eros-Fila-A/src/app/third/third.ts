40836ef2282bd203b93d211b115295eedf047334
brancaccioeros <brancaccio.eros@iisgalvanimi.edu.it>
2026-02-13 10:57:06 +0100

    Update third.ts

--- Brancaccio-Eros-Fila-A/src/app/third/third.ts
+++ Brancaccio-Eros-Fila-A/src/app/third/third.ts
@@ -2,10 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-third',
-  imports: [],
   templateUrl: './third.html',
   styleUrl: './third.css',
 })
 export class Third {
-
+  tuo_nome: string = 'terzo';
 }
