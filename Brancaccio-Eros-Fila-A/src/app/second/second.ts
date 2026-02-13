2f6804d7238f632348dd2c111c2aead263c75838
brancaccioeros <brancaccio.eros@iisgalvanimi.edu.it>
2026-02-13 10:55:53 +0100

    Update second.ts

--- Brancaccio-Eros-Fila-A/src/app/second/second.ts
+++ Brancaccio-Eros-Fila-A/src/app/second/second.ts
@@ -2,10 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-second',
-  imports: [],
   templateUrl: './second.html',
   styleUrl: './second.css',
 })
 export class Second {
+  tuo_nome: string = 'secondo';
 
 }
